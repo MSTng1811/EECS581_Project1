7756df22be2bf2bdf034424d85a4ce2d6720a1a9
MSTng1811 <157640121+MSTng1811@users.noreply.github.com>
2024-09-11 11:40:58 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,99 @@
+import base64
+import os
+import time
+import numpy as np
+import threading
+import socket
+import pygame
+
+class Server:
+
+    def __init__(self, screen, server, connect=None, add=None):
+        self.screen = screen
+        self.server = server
+        self.connect = connect
+        self.add = add
+        self.start = False
+        self.data = None
+
+    def createServer(self):			#Function to establish a server in order for clients to join and connect with the game
+        PORT = 8080	#Default port for web servers
+	#Here we are creating a socket stream to establish a network, with AF_INET meaning IPv4 protocol, and SOCK_STREAM establishing TCP protocol
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind(('', PORT))	#Binding the port with the established network stream
+        self.server.listen()		#listen() will help us to access this port via the communication protocol we set up
+
+    def getInvite(self):		#Function to generate a team invite code (IP address) to connect to the multiplayer game
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)		#Here we are establishing a UDP protocol for data sending
+        s.connect(("8.8.8.8", 80))		#Default IP
+        font = pygame.font.SysFont("arial", 72)			#CHANGE FONT HERE IF YOU WANT TO
+        smallFont = pygame.font.SysFont("arial", 60)		#CHANGE FONT HERE
+        IP = s.getsockname()[0]		#Socket function to get the IP address for the established connection
+        enc = ""
+        for i in IP:
+            if i == ".":
+                enc+= "#"		#Since IP address is in the form of 0.0.0.0, we are replacing . with # symbol here
+            else:
+                enc+= chr(98+int(i))	#In this line, we are replacing the numbers with their ASCII values
+        code = smallFont.render(str(enc), True, (255, 255, 255))	#Color of generated code- White (255, 255, 255) CHANGE COLOR HERE IF YOU WANT TO
+        heading = font.render("Battle Code", True, (255, 255, 255))	#Color of heading- White (255, 255, 255) CHANGE COLOR HERE IF YOU WANT TO
+        self.screen.blit(heading, (900, 30))
+        self.screen.blit(code, (900, 120))
+
+    def username(self, name):		#Function to print username
+        font = pygame.font.SysFont("arial", 36)		#CHANGE FONT STYLE AND SIZE HERE
+        heading = font.render(name, True, (255, 255, 255))
+        self.screen.blit(heading, (100, 325))
+
+    def genVers(self):		#Generate Versus text
+        font = pygame.font.SysFont("arial", 72)
+        heading = font.render("VERSUS", True, (255, 255, 255))
+        self.screen.blit(heading, (420, 305))
+
+    def enemyUsername(self, data):		#Displaying enemy username
+        font = pygame.font.SysFont("arial", 36)
+        if not data:
+            head = font.render("Pending....", True, (255, 255, 255))
+        else:
+            head = font.render(str(data), True, (255, 255, 255))
+        self.screen.blit(head, (950, 325))
+
+    def heading(self):		#Generating the heading of the game
+        font = pygame.font.SysFont("arial", 72)
+        heading = font.render("Battle-Ship", True, (255, 255, 255))
+        self.screen.blit(heading, (30, 30))
+
+    def beginBattle(self):
+        button = pygame.image.load("pictures/begin.png")
+        self.screen.blit(button, (210, 450))
+
+    def run(self, username, first, second, changeLoc, sendShip):
+        if first==True:
+            self.screen.fill((255, 255, 255))
+            self.getInvite()
+            self.username(name=username)
+            self.createServer()
+            self.heading()
+            self.beginBattle()
+            self.genVers()
+            self.enemyUsername(None)
+            pygame.display.update()
+        elif not first and second:
+            self.connect, self.add = self.server.accept()
+            data = self.connect.recv(1024)
+            if data:
+                self.screen.fill((255, 255, 255))
+                self.getInvite()
+                self.username(name=username)
+                self.heading()
+                self.beginBattle()
+                self.genVers()
+                self.enemyUsername(data=data.decode("utf-8"))
+                pygame.display.update()
+                self.start = True
+                self.data = data
+                return False
+        if sendShip:
+            print("sent")
+            self.connect.send(bytes("Place Ships", "utf-8"))
+        return True
---
+++ setup.py
@@ -0,0 +1,161 @@
+import pygame
+import socket
+import numpy as np
+import math
+
+
+class BoardSetup:
+
+    def __init__(self, screen, board):
+        self.screen = screen
+        self.board = board
+        self.rotated = False
+        self.submarine = pygame.image.load("pictures/submarine.png")
+        self.cruiser = pygame.image.load("pictures/cruiser.png")
+        self.carrier = pygame.image.load("pictures/carrier.png")
+        self.battleship = pygame.image.load("pictures/battleship.png")
+        self.destroyer = pygame.image.load("pictures/destroyer.png")
+        self.destCount = 1
+        self.crusCount = 1
+        self.carrCount = 1
+        self.batCount = 1
+        self.subCount = 1
+        self.start = pygame.image.load("pictures/start.png")
+        self.start = pygame.transform.scale(self.start, (480, 360))
+        self.offset = 0;
+
+    def generateGrid(self):		#Function to generate 10x10 grid board
+        letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+        for x, i in enumerate(self.board):	#x: Index, i: Value
+            for y, j in enumerate(i):		#y: Index, j: Value
+                if j == 0:		#Implying the grid is empty, thus creating a small box with white border and a circle in between
+                    pygame.draw.rect(self.screen, (255, 255, 255), (600+(65*y), 63+self.offset, 63, 63), 1)
+                    pygame.draw.circle(self.screen, (255, 255, 255), ((600+(65*y))+30, (63+self.offset)+30), 6)
+                else:			#Implying the grid is not empty and a ship is placed there, thus creating a green filled box with circle in between
+                    pygame.draw.rect(self.screen, (25, 209, 83), (600+(65*y), 63+self.offset, 63, 63), 1)
+                    pygame.draw.circle(self.screen, (25, 209, 83), ((600+(65*y))+30, (63+self.offset)+30), 6)
+            self.offset+=65		#Increasing the cell offset value to change the coordinates and thus create equal grids in different rows
+        self.offset = 0		#Resetting offset value
+
+        #After generating the grid, we will label the grids with the index values by iterating through each grid position
+        for x, i in enumerate(self.board):
+            font = pygame.font.Font("arial", 48)
+            num = font.render(str(x+1), True, (255, 255, 255))
+            self.screen.blit(num, (610+(x*65), 8))		#Placing the numbers on each grid horizontally
+            letter = font.render(letters[x], True, (255,255,255))
+            self.screen.blit(letter, (570, 60+(x*65)))		#Placing the alphabets on each grid vertically
+
+    def shipSet(self):		#Function to place ship icons on the game screen
+        font = pygame.font.Font("arial", 72)
+        #Setting font for count value of ships
+        destCount = font.render(str(self.destCount), True, (255, 255, 255))
+        crusCount = font.render(str(self.crusCount), True, (255, 255, 255))
+        subCount = font.render(str(self.subCount), True, (255, 255, 255))
+        batCount = font.render(str(self.batCount), True, (255, 255, 255))
+        carrCount = font.render(str(self.carrCount), True, (255, 255, 255))
+        #Positioning the images and generated texts on the game screen
+        self.screen.blit(self.submarine, (30, 425))
+        self.screen.blit(self.cruiser, (30, 300))
+        self.screen.blit(self.carrier, (30, 600))
+        self.screen.blit(self.battleship, (30, 500))
+        self.screen.blit(self.destroyer, (30, 200))
+        self.screen.blit(subCount, (350, 415))
+        self.screen.blit(crusCount, (325, 325))
+        self.screen.blit(carrCount, (500, 615))
+        self.screen.blit(batCount, (400, 515))
+        self.screen.blit(destCount, (225, 215))
+
+    def click(self, image, event):		#Function to load the image to mouse to be able to drag it around screen
+        x, y = pygame.mouse.get_pos()
+        key = pygame.key.get_pressed()
+        if image != "":			#If image is selected, then perform relevant actions
+            if key[pygame.K_LEFT] and not self.rotated:
+                image = pygame.transform.rotate(image, 90)
+                self.rotated = True
+            elif self.rotated == True:
+                image = pygame.transform.rotate(image, 90)
+                if key[pygame.K_LEFT]:
+                    image = pygame.transform.rotate(image, -90)
+                    self.rotated = False
+            width, height = image.get_size()
+            self.run()
+            self.screen.blit(image, (x-width/2, y-height/2))
+            pygame.display.update()
+
+    def generatePos(self):	#Function to generate coordinate values for the grid board
+        x, y = 600, 30		#Initial coordinates
+        grid = np.zeros(shape=(10, 10), dtype=tuple)		#Creating a grid tuple
+        cellMap = 0
+        #Storing the value of each grid to be able to place the ships
+        for m, i in enumerate(self.board):
+            for n, j in enumerate(i):
+                x += (n*65)
+                y += (cellMap*65)
+                pos = (x,y)
+                grid[m][n] = pos
+            cellMap += 1
+        return grid
+
+    def calcPos(self, ship):	#Function to calculate the position on grid and performing necessary functions
+        x, y = pygame.mouse.get_pos()		#Getting mouse coordinates
+        x = math.floor((x-600)/65)
+        y = math.floor((y-30)/65)
+        if x>= 0 and y>= 0:
+	    #Positioning the ships on the board. Here we are marking the grid coordinates as 1 which will be passed to the generateGrid() function
+	    #And that will color the occupied grids as green. If the ship is rotated, then vertical values will be changed, ad vice versa for horizontal
+            if ship == "destroyer":	#GRID SIZE- 3
+                if self.rotated:
+                    self.board[y-1][x] = 1
+                    self.board[y][x] = 1
+                    self.board[y+1][x] = 1
+                else:
+                    self.board[y][x-1:x+2] = 1
+                self.destCount = 0
+            elif ship == "cruiser":	#GRID SIZE- 4
+                if self.rotated:
+                    self.board[y-2][x] = 1
+                    self.board[y-1][x] = 1
+                    self.board[y][x] = 1
+                    self.board[y+1][x] = 1
+                else:
+                    self.board[y][x - 2:x + 2] = 1
+                self.crusCount = 0
+            elif ship == "carrier":	#GRID SIZE- 7
+                if self.rotated:
+                    self.board[y - 3][x] = 1
+                    self.board[y - 2][x] = 1
+                    self.board[y - 1][x] = 1
+                    self.board[y][x] = 1
+                    self.board[y + 1][x] = 1
+                    self.board[y + 2][x] = 1
+                    self.board[y + 3][x] = 1
+                else:
+                    self.board[y][x - 3:x + 4] = 1
+                self.carrCount = 0
+            elif ship == "submarine" or ship == "battleship":	#GRID SIZE- 5
+                if self.rotated:
+                    self.board[y-2][x] = 1
+                    self.board[y - 1][x] = 1
+                    self.board[y][x] = 1
+                    self.board[y + 1][x] = 1
+                    self.board[y + 2][x] = 1
+                else:
+                    self.board[y][x - 2:x + 3] = 1
+                if ship == "submarine":
+                    self.subCount = 0
+                else:
+                    self.batCount = 0
+            return True
+
+    def run_first(self):	#Function combining the whole class and running it together
+        #Setting the screen background
+        self.screen.fill((0, 0, 0))
+        font = pygame.font.SysFont("arial", 72)
+        heading = font.render("Place your ships", True, (255, 255, 255))
+        self.screen.blit(heading, (30, 30))
+	#Calling other defined functions
+        self.generateGrid()
+        self.shipSet()
+	#The condition below makes sures that all the ships are positioned before the game starts
+        if self.carrCount==0 and self.batCount==0 and self.subCount==0 and self.crusCount==0 and self.destCount==0:
+            self.screen.blit(self.start, (-90, 70))
